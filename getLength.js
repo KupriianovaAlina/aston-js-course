b497213c9e211c49008dd03f1980542a75c70745
alina <akupriianova99@gmail.com>
2024-02-19 11:46:43 +0300

    ASTONJS-1 [fix]: add missing types check

--- getLength.js
+++ getLength.js
@@ -4,11 +4,6 @@ function getLength(item) {
     console.log(item.length);
     return;
   }
-  // число
-  if (typeof item === "number") {
-    console.log(String(item).length);
-    return;
-  }
   // null
   if (item === null) {
     console.log(0);
@@ -16,6 +11,16 @@ function getLength(item) {
   }
   // объект
   if (typeof item === "object") {
+    // set, map
+    if (item instanceof Set || item instanceof Map) {
+      console.log(item.size);
+      return;
+    }
+    // псевдомассив
+    if (typeof item.length === 'number' && Number(item.length) >= 0) {
+      console.log(Array.from(item).length);
+      return;
+    }
     console.log(Object.keys(item).length);
     return;
   }
@@ -23,11 +28,31 @@ function getLength(item) {
   console.log(0);
 }
 
-getLength("Hello, world!"); // Длина строки: 13
-getLength(12345); // Длина числа: 5
-getLength([1, 2, 3, 4]); // Длина массива: 4
-getLength({ name: "Name", length: 4 }); // Количество свойств объекта: 2
-getLength(true); // Длина значения: 0
-getLength(undefined); // Длина значения: 0
-getLength(null); // Длина значения: 0
-getLength(NaN); // Длина значения: 0
+// строка, длина 13
+getLength("Hello, world!");
+// массив, длина 4
+getLength([1, 2, 3, 4]);
+// объект, длина 2
+getLength({ name: "Name", surname: "Smith" });
+// set, длина 5
+getLength(new Set([1, 2, 3, 3, 4, 5]))
+// map, длина 3
+getLength(new Map([["огурец", 500], ["помидор", 350], ["лук", 50]]));
+// псевдомассив, длина 3
+getLength({ 0: 1, 1: 2, 2: 3, length: 3 });
+// number, длина 0
+getLength(12345);
+// boolean, длина 0
+getLength(true);
+// undefinded, длина 0
+getLength(undefined);
+// nul, длина 0
+getLength(null);
+// NaN, длина 0
+getLength(NaN);
+// function, длина 0
+getLength(() => { console.log('я функция') });
+// date, длина 0
+getLength(new Date());
+// infinity, длина 0
+getLength(Infinity);
